6d1a2fd4b728711cc3f804f2b10e888dc6469f5b
Saulzu <124089079+Saulzu@users.noreply.github.com>
2026-02-02 18:07:30 +0000

    complete_tasks

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
+#include <stdio.h>
 
-int main()
+int main() 
 {
-    int precio = 10;
-    int cuenta = 0;
-
-    cuenta = precio + 5;
-    printf("El valor de cuenta es %d", cuenta);
+        float Peso, Altura;
+        printf("Introduce tu Peso:");
+        scanf("%f", &Peso);
+        printf("Introduce tu Altura:");
+    scanf("%f", &Altura);
+    float ideal = 0.0;
+    float IMC = Peso / ((Altura)*(Altura));
+    if (IMC<25) {
+        ideal = 25 - IMC ;
+        printf("Debe subir %f kg", ideal);
+    }
+    else {
+        ideal = IMC - 25 ;
+        printf("Debes bajar %f kg", ideal);
+    }
     return 0;
+    
 }
